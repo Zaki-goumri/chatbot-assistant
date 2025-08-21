cc05678bd7f1dc5bef1e393c79c75eb06bbddcaf
zaki <z.goumri@esi-sba.dz>
2025-08-21 18:42:52 +0100

    fix(cors):add portfolio route to cors

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from route import router
 import gc
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://zakariagoumri.vercel.app/chat"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
